858829ddf915cbbe80d8f3f5aba4853756ebae79
V21 <76143641+StandardV@users.noreply.github.com>
2023-02-07 11:40:55 -0600

    Update ApplicationLi.py

--- ApplicationLi.py
+++ ApplicationLi.py
@@ -14,6 +14,7 @@ class wind(ct.CTk):
 
         self.title("RandomBullshatGO")
         self.geometry(f"{1100}x{580}")
+        #self.resizable(False, False)
  
         self.grid_rowconfigure(0,weight=1)
         self.grid_columnconfigure((0,1,2,3), weight=1)
@@ -44,16 +45,16 @@ class wind(ct.CTk):
         #secondframe
         self.MainFrame= ct.CTkFrame(self,fg_color='transparent')
         self.MainFrame.grid(row=0,column=1,sticky='nsew')
-        self.MainFrame.grid_rowconfigure((0,1,3), weight=0)
+        self.MainFrame.grid_rowconfigure((0,1,6), weight=1)
         self.MainFrame.grid_columnconfigure((0), weight=1)
         self.sortratio=ct.CTkOptionMenu(master=self.MainFrame, values=["Not Yet Applied", "Nearest Reset Time","Applied", "Furthest Reset Time","Lexicographically"],command=self.sortevent)
-        self.sortratio.grid(row=0,column=0,pady=20,sticky='n')
+        self.sortratio.grid(row=0,column=0,pady=(20,0),sticky='n')
         self.typer= ct.CTkTextbox(master=self.MainFrame,corner_radius=10,height=350)
-        self.typer.grid(row=1,column=0,padx=(10,10),sticky='nsew')
+        self.typer.grid(row=1,column=0,rowspan=3,padx=(10,10),sticky='nsew')
         self.submit1=ct.CTkButton(master=self.MainFrame,text="Submit",corner_radius=10)
-        self.submit1.grid(row=3,column=0,pady=10,sticky='ns')
+        self.submit1.grid(row=5,column=0,pady=30,sticky='ns')
         self.Progress1=ct.CTkProgressBar(master=self.MainFrame)
-        self.Progress1.grid(row=4,column=0,padx=10,pady=20,sticky='nsew')
+        self.Progress1.grid(row=6,column=0,padx=10,pady=30,sticky='s')
         self.Progress1.configure(mode="indeterminnate") 
         self.Progress1.start()
 
