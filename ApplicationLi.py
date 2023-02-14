0c55a84c3d782b7c9471c402bfa5c2e77c742455
V21 <76143641+StandardV@users.noreply.github.com>
2023-02-13 19:52:41 -0600

    Update ApplicationLi.py
    Full version update of Application.py

--- ApplicationLi.py
+++ ApplicationLi.py
@@ -1,21 +1,23 @@
-import datetime
 import tkinter
 import tkinter.messagebox
 import customtkinter as ct
-import os
+from ctk_entryframe import CTkEntryFrame
+import ApplicationSorter
 import pickle
 import time
 
 Waittime=6480000#Time Range that program will toggled off already marked companies by itself so that you could reapply
+States='Lexicographically'
 
 ct.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 ct.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 dictoggle={};dicposition={};dictime={}#for the purpose of sorting and toggling logic
 GroupedLi=[]
+
 class wind(ct.CTk):
     
     def __init__(self):
-        global dictoggle,dicposition,dictime,GroupedLi
+        global dictoggle,dicposition,dictime,GroupedLi,States
         super().__init__()
         
         self.title("RandomBullshatGO")
@@ -41,8 +43,8 @@ class wind(ct.CTk):
         self.scrollable_frame_switches = []
         #scrollable list content
         with open(r"D:\Largecodefile\TkinterApply\List1.txt",'r') as f:
-            Lines=f.read().splitlines()        
-        for i,val in enumerate(Lines):
+            self.Lines=f.read().splitlines()        
+        for i,val in enumerate(self.Lines):
             switch = ct.CTkSwitch(master=self.scrollable_frame, text=val,command=lambda charac=val , index=i: self.changestate(charac,index))#lambda index=i: self.changestate(index)
             switch.grid(row=i, column=0, padx=10, pady=(0, 20))
             self.scrollable_frame_switches.append(switch)
@@ -53,11 +55,11 @@ class wind(ct.CTk):
         self.MainFrame.grid(row=0,column=1,sticky='nsew')
         self.MainFrame.grid_rowconfigure((0,1,5,6,7), weight=1)
         self.MainFrame.grid_columnconfigure((0), weight=1)
-        self.sortratio=ct.CTkOptionMenu(master=self.MainFrame, values=["Not Yet Applied", "Nearest Reset Time","Applied", "Furthest Reset Time","Lexicographically"],command=self.sortevent)
+        self.sortratio=ct.CTkOptionMenu(master=self.MainFrame, values=["Lexicographically","Not Yet Applied","Applied"],command=self.sortevent)
         self.sortratio.grid(row=0,column=0,pady=(20,0),sticky='n')
         self.typer= ct.CTkTextbox(master=self.MainFrame,corner_radius=10,height=350)
         self.typer.grid(row=1,column=0,rowspan=3,padx=(10,10),sticky='nsew')
-        self.submit1=ct.CTkButton(master=self.MainFrame,text="Submit",corner_radius=10)
+        self.submit1=ct.CTkButton(master=self.MainFrame,text="Submit",corner_radius=10,command=self.trigger_testing)
         self.submit1.grid(row=5,column=0,pady=10,sticky='ns')
         self.DiagBox1 = ct.CTkButton(master=self.MainFrame, text="Delete Content",corner_radius=10, command=self.Delcontent)
         self.DiagBox1.place(relx=0.5, rely=0.5, anchor=ct.CENTER)
@@ -89,23 +91,67 @@ class wind(ct.CTk):
             GroupedLi=pickle.load(f)
         times=time.time()
         dictoggle,dicposition,dictime=GroupedLi[0],GroupedLi[1],GroupedLi[2]
-        scanli1=[i for i in dictoggle if dictoggle[i] == True]#get key in dic with specific value IE (if got toggle on: in this case 1)
-        scanli2=[i for i in dictime if dictime[i] !=0 and times>dictime[i]]
-        if len(scanli1) !=0:
-            for i in scanli1:
+        self.scanli1=[i for i in dictoggle if dictoggle[i] == True]#get key in dic with specific value IE (if got toggle on: in this case 1)
+        self.scanli2=[i for i in dictime if dictime[i] !=0 and times>dictime[i]]
+        if len(self.scanli1) !=0:
+            for i in self.scanli1:
                 self.scrollable_frame_switches[dicposition[i]].select()
-        if len(scanli2) !=0:
-            print(scanli2)
-            for i in scanli2:
-                dictime[i]=0
-                log=f'Toggled off value of {str(i)} since the amount of time f{str(Waittime)} has been met\n\n'
+        if len(self.scanli2) !=0: 
+            print(self.scanli2)
+            for i in self.scanli2:
+                #dictime[i]=0
+                log=f'System: Toggled off value of {str(i)} since the amount of time {str(Waittime)} has been met\n\n'
                 self.logging(log)
-                self.scrollable_frame_switches[dicposition[i]].toggle()#deselect if specific time of daterange meet: in this case, set as 75 days from the time of checking     
+                self.scrollable_frame_switches[dicposition[i]].toggle()#deselect if specific time of daterange meet: in this case, set as 75 days from the time of checking   
 
+
+    def trigger_testing(self):
+        global dictoggle,dicposition,dictime
+        if len(self.typer.get("1.0",'end-1c'))>0:
+            temp=[part for part in self.typer.get("1.0",'end-1c').split('\n') if part != '']#list comprehension to extract list from provided data that are turned into string by CTk
+            print(temp)
+            with open(r"D:\Largecodefile\TkinterApply\List1.txt",'a') as f:
+                for i in temp:
+                    f.write(f'{i}\n')
+                    dictoggle[i]=False;dictime[i]=0#modify dictime and dictoggle
+            log=f'Added {temp} to the System\n\n'
+            self.logging(log)
+            self.typer.delete("1.0",'end')
+            self.Lines, dicposition=ApplicationSorter.sortLexico()#ADD IN Dicposition
+            Li=[dictoggle,dicposition,dictime]
+            with open(r"D:\Largecodefile\TkinterApply\dictionstoring.txt", "wb") as k:
+                print('accessed')
+                pickle.dump(Li, k)
+            self.sortevent(States)
+            
+        #self.logging()
     def sortevent(self,currentval):# Sorting
-        print(currentval)
-        return currentval
-    
+        global States
+        States= currentval
+        self.scanli1=[i for i in dictoggle if dictoggle[i] == True]
+        if currentval == 'Lexicographically':
+            temp=self.Lines
+        elif currentval == 'Applied':    
+            temp=self.scanli1
+        else:
+            temp=[i for i in self.Lines if i not in self.scanli1]
+        self.scrollable_frame.destroy()
+        self.scrollable_frame = ct.CTkScrollableFrame(master=self.SideFrame, label_text="Application List")
+        self.scrollable_frame.grid(row=1, column=0,rowspan=4,columnspan=4, padx=(0, 0), pady=(10, 0), sticky="nsew")
+        self.scrollable_frame.grid_columnconfigure(0, weight=1)
+        self.scrollable_frame_switches = []   
+        tempLi={}
+        for i,val in enumerate(temp):   
+            tempLi[val]= i
+        for i,val in enumerate(temp):
+            switch = ct.CTkSwitch(master=self.scrollable_frame, text=val,command=lambda charac=val , index=i: self.changestate(charac,index))#lambda index=i: self.changestate(index)
+            switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+            self.scrollable_frame_switches.append(switch)
+        if len(self.scanli1) !=0 and currentval != 'Not Yet Applied':
+            for i in self.scanli1:
+                self.scrollable_frame_switches[tempLi[i]].select()
+
+
     def logging(self, logger):
         with open(r"D:\Largecodefile\TkinterApply\Logging.txt",'a') as f:#change from 'a' to 'w' if prefre one-time logfile, IE session log, meaning full log will not exist when program end
             self.SideBox1.configure(state='normal')
@@ -119,15 +165,38 @@ class wind(ct.CTk):
         ct.set_appearance_mode(setc.lower())
     
     def Delcontent(self):#Content Delete Function
-        dialog = ct.CTkInputDialog(text="Input Company Name:", title="Janitor")
-        print("Return Val:", dialog.get_input())
-
-    def changestate(self,val,index):#toggle switch 
+        global dictoggle,dicposition,dictime
+        while True:
+            dialog = CTkEntryFrame(text="Input Company Name:", title="Janitor")#CTkInputDialog(text="Input Company Name:", title="Janitor")
+            temp=''
+            temp=dialog.get_input()
+            if temp != None:
+                if temp != '' or temp != ' ':
+                    if dicposition.get(temp,-1) !=-1:
+                        dictime.pop(temp);dictoggle.pop(temp)
+                        self.Lines,dicposition=ApplicationSorter.delvalue(temp)
+                        log=f'Deleted all instances of "{temp}" from the System\n\n'
+                        self.logging(log)
+                        Li=[dictoggle,dicposition,dictime]
+                        with open(r"D:\Largecodefile\TkinterApply\dictionstoring.txt", "wb") as k:
+                            print('accessed')
+                            pickle.dump(Li, k)
+                        self.sortevent(States)
+                else:
+                    print('No value returned')
+            else:
+                print('value canceled')
+                break
+        # COME BACK HERE SORT OUT THE DIAGLOGE KEEP DISAPPEAR AFTER PRESS OKAY
+    
+    def changestate(self,val,index):#toggle switch
         dictoggle[val]=not dictoggle[val]
         log=f'Switched value of {str(val)} to {str(dictoggle[val])}\n\n'
         self.logging(log)
         if dictoggle[val] == True:
             dictime[val]=time.time()+Waittime
+        else:
+            dictime[val]=0
         GroupedLi=[dictoggle,dicposition,dictime]
         with open(r"D:\Largecodefile\TkinterApply\dictionstoring.txt", "wb") as f:
             pickle.dump(GroupedLi, f)
